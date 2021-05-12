327776a0230bdd572db78f3c90c4f82177c7ecee
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2021-05-12 01:06:25 -0700

    ir reshuffle

--- test/emit.js
+++ test/emit.js
@@ -6,29 +6,39 @@ const lib = require('../lib/index.js');
 
 const expect = chai.expect;
 
+const $ = lib.elements;
+
+const bar = $.module('bar');
+
+Object.assign(bar, {
+  inputo: {
+    inp1: {width: 1},
+    inp2: {width: 32}
+  },
+  outputo: {
+    out1: {width: 11}
+  },
+  signalo: {
+    tmp1: {type: 'wire', width: 1}
+  },
+  items: [
+    {type: 'comment', value: 'some more comment'},
+    {type: 'assign', lhs: 'out1', rhs: {type: 'and', items: ['inp1', 'tmp1', 'tmp2']}}
+  ]
+});
+
 const testo = {
   t1: {
     ir: (
-      {type: 'circuit', name: 'foo', items: [
-        {type: 'module', name: 'bar', items: [
-          {type: 'io', items: [
-            {type: 'input', items: [{type: 'Int', id: 'inp1', width: 1}]},
-            {type: 'comment', value: 'some comment'},
-            {type: 'output', items: [{type: 'Int', id: 'out1', width: 11}]}
-          ]},
-          {type: 'wire', items: [{type: 'Int', id: 'tmp1'}]},
-          {type: 'comment', value: 'some more comment'},
-          {type: 'assign', lhs: 'out1', rhs: {type: 'and', items: ['inp1', 'tmp1', 'tmp2']}}
-        ]}
-      ]}
+      {type: 'circuit', name: 'foo', items: [bar]}
     ),
     fir: [
       'circuit foo :',
       '  module bar :',
       '    input  inp1: UInt<1>',
-      '    ; some comment',
+      '    input  inp2: UInt<32>',
       '    output out1: UInt<11>',
-      '    wire   tmp1: UInt',
+      '    wire   tmp1: UInt<1>',
       '    ; some more comment',
       '    out1 <= and(inp1, and(tmp1, tmp2))'
     ],
@@ -37,7 +47,7 @@ const testo = {
       'module bar',
       '(',
       '  input              inp1,',
-      '  // some comment',
+      '  input       [31:0] inp2,',
       '  output      [10:0] out1',
       ');',
       'wire               tmp1;',
