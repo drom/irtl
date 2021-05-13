640df622857de15a83114680d2014c237992ade2
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2021-05-12 23:05:59 -0700

    large IR reshuffle

--- test/emit.js
+++ test/emit.js
@@ -11,19 +11,18 @@ const $ = lib.elements;
 const bar = $.module('bar');
 
 Object.assign(bar, {
-  inputo: {
-    inp1: {width: 1},
-    inp2: {width: 32}
-  },
-  outputo: {
-    out1: {width: 11}
-  },
-  signalo: {
-    tmp1: {type: 'wire', width: 1}
+  defo: {
+    inp1: {width: 1,  dir: 'input'},
+    inp2: {width: 32, dir: 'input'},
+    out1: {width: 11, dir: 'output'},
+    tmp1: {width: 1},
+    tmp2: {width: 8,  clock: 'clk'},
+    tmp3: {width: 16, clock: 'clk', reset: 'rst', sync: true},
+    tmp4: {width: 32, clock: 'clk', reset: 'arst', resetValue: 1}
   },
   items: [
     {type: 'comment', value: 'some more comment'},
-    {type: 'assign', lhs: 'out1', rhs: {type: 'and', items: ['inp1', 'tmp1', 'tmp2']}}
+    {type: 'assign', dst: 'out1', src: {type: 'and', items: ['inp1', 'tmp1', 'tmp2']}}
   ]
 });
 
@@ -39,6 +38,9 @@ const testo = {
       '    input  inp2: UInt<32>',
       '    output out1: UInt<11>',
       '    wire   tmp1: UInt<1>',
+      '    reg    tmp2: UInt<8>, clk',
+      '    reg    tmp3: UInt<16>, clk with: (reset => (rst, 0))',
+      '    reg    tmp4: UInt<32>, clk with: (reset => (arst, 1))',
       '    ; some more comment',
       '    out1 <= and(inp1, and(tmp1, tmp2))'
     ],
@@ -51,6 +53,9 @@ const testo = {
       '  output      [10:0] out1',
       ');',
       'wire               tmp1;',
+      'reg          [7:0] tmp2;',
+      'reg         [15:0] tmp3;',
+      'reg         [31:0] tmp4;',
       '// some more comment',
       'assign out1 = (inp1 & tmp1 & tmp2)',
       'endmodule',
